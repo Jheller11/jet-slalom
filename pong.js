48aab30dccb12dc23f5d9714ef281d4aa66205e5
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-10-02 22:14:01 -0400

    add component type, add ball

--- pong.js
+++ pong.js
@@ -1,12 +1,16 @@
-let playerPaddle, pcPaddle, ball
+let playerPaddle,
+  pcPaddle,
+  ball,
+  walls = []
 
 class Component {
-  constructor(width, height, color, x, y) {
+  constructor(width, height, color, x, y, type) {
     this.width = width
     this.height = height
     this.color = color
     this.x = x
     this.y = y
+    this.type = type
     this.speedX = 0
     this.speedY = 0
   }
@@ -18,8 +22,26 @@ class Component {
   }
 
   newPos() {
-    this.x += this.speedX
-    this.y += this.speedY
+    if (this.y + this.speedY > 400) {
+      this.y = 400
+    } else if (this.y + this.speedY < 0) {
+      this.y = 0
+    } else {
+      this.x += this.speedX
+      this.y += this.speedY
+    }
+  }
+
+  moveUp() {
+    if (this.speedY < 5) {
+      this.speedY += 5
+    }
+  }
+
+  moveDown() {
+    if (this.speedY > -5) {
+      this.speedY -= 5
+    }
   }
 }
 
@@ -30,7 +52,7 @@ var gameCanvas = {
     this.canvas.height = 500
     this.context = this.canvas.getContext('2d')
     document.querySelector('.game').appendChild(this.canvas)
-    this.interval = setInterval(updateGameArea, 20)
+    this.interval = setInterval(updateGameArea, 25)
     this.frameNo = 0
     this.score = [0, 0]
   },
@@ -44,8 +66,20 @@ var gameCanvas = {
 
 const initiateGame = () => {
   gameCanvas.start()
-  playerPaddle = new Component(20, 100, 'blue', 50, 200)
-  pcPaddle = new Component(20, 100, 'red', 830, 200)
+  playerPaddle = new Component(20, 100, 'blue', 50, 200, 'paddle')
+  pcPaddle = new Component(20, 100, 'red', 830, 200, 'paddle')
+  walls.push(new Component(900, 10, 'gray', 0, 0, 'wall'))
+  walls.push(new Component(900, 10, 'gray', 0, 490, 'wall'))
+  ball = new Component(10, 10, 'green', 445, 245, 'ball')
+  ball.speedX = 3
+  ball.speedY = 5
+  document.addEventListener('keydown', e => {
+    if (e.keyCode == '40') {
+      playerPaddle.moveUp()
+    } else if (e.keyCode == '38') {
+      playerPaddle.moveDown()
+    }
+  })
 }
 
 const updateGameArea = () => {
@@ -54,6 +88,11 @@ const updateGameArea = () => {
   pcPaddle.newPos()
   playerPaddle.update()
   pcPaddle.update()
+  ball.newPos()
+  ball.update()
+  walls.forEach(wall => {
+    wall.update()
+  })
 }
 
 initiateGame()
